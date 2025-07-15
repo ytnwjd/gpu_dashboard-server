02c35bcb129770b9af635b52fce137f658f4cb8d
y.tnwjd <ysjsw@ajou.ac.kr>
2025-07-16 01:45:47 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import fastapi as fastapi
+
+app = fastapi.FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
