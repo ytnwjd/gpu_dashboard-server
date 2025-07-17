095ef3f5e1e3b1faf0bd91cdb278f4981bf15655
y.tnwjd <ysjsw@ajou.ac.kr>
2025-07-17 18:51:16 +0900

    feat: response model

---
+++ models.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import List, Optional, Any
+
+class ApiResponse(BaseModel):
+    code: int
+    message: str
+    data: Optional[Any] = None # data 필드는 어떤 타입이든 될 수 있으므로 Any 사용
+
+class Job(BaseModel):
+    id: str
+    timestamp: str
+    jobName: str
+    logs: Optional[List[str]] = None
+
+class JobListResponse(ApiResponse):
+    data: Optional[List[Job]] = None
