eb61060a7c35e7368e6355411c6ba03107d7f7c2
y.tnwjd <ysjsw@ajou.ac.kr>
2025-07-17 19:23:33 +0900

    chore: add jobs route

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from api import jobs 
 
 app = FastAPI()
 
@@ -17,22 +18,10 @@ app.add_middleware(
     allow_headers=["*"],  # 모든 헤더 허용
 )
 
+app.include_router(jobs.router)
+
 def read_root():
     return {"message": "Hello, World!"}
-
-
-@app.get("/api/jobs")
-async def get_jobs():
-    
-    job_list = [
-        {"id": "1", "timestamp": "2025-07-14 10:00:00", "jobName": "My first GPU job"},
-        {"id": "2", "timestamp": "2025-07-14 10:15:30", "jobName": "Deep Learning Model Training"},
-        {"id": "3", "timestamp": "2025-07-15 10:15:30", "jobName": "Deep Learning Job C"},
-        {"id": "4", "timestamp": "2025-07-15 11:00:00", "jobName": "Data Preprocessing"},
-        {"id": "5", "timestamp": "2025-07-16 09:30:00", "jobName": "Image Classification"},
-        {"id": "6", "timestamp": "2025-07-17 12:00:00", "jobName": "NLP Model Training", "logs": ["Log for job 6 line 1", "Log for job 6 line 2", "Log for job 6 line 3", "Log for job 6 line 4"]},
-    ]
-    return job_list
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
