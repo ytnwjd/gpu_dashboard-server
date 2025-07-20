0795f34c66a0bdb63efe60bfd1a99604d0975ac7
y.tnwjd <ysjsw@ajou.ac.kr>
2025-07-20 23:00:39 +0900

    fix: fix field name

--- services/gpu_service.py
+++ services/gpu_service.py
@@ -20,7 +20,7 @@ class GpuService:
             "gpu8gbActive": active_8gb,
             "gpu24gbAvailable": available_24gb,
             "gpu8gbAvailable": available_8gb,
-            "jobs_in_queue": jobs_in_queue,
+            "jobsInQueue": jobs_in_queue,
         }
         
         return GpuStatus(**gpu_status)
