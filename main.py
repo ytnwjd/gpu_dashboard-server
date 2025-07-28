1f533a95b116e5f18c56c85d8c26dad849133610
y.tnwjd <ysjsw@ajou.ac.kr>
2025-07-28 18:09:21 +0900

    feat: add file router

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from api import jobs, gpu
+from api import jobs, gpu, file
 
 app = FastAPI()
 
@@ -20,6 +20,7 @@ app.add_middleware(
 
 app.include_router(jobs.router)
 app.include_router(gpu.router)
+app.include_router(file.router)
 
 def read_root():
     return {"message": "Hello, World!"}
