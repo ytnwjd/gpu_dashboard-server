09a36d2d3531fa5c2bb5d4a63eeaca62850a7f2d
y.tnwjd <ysjsw@ajou.ac.kr>
2025-08-05 20:01:20 +0900

    feat: import dotenv

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+
+from dotenv import load_dotenv
 from api import jobs, gpu, file
 
+load_dotenv() 
 app = FastAPI()
 
 # CORS 
