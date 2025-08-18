b7507ee23be9ecffee0b5c67c0d0bbf7c8f66255
y.tnwjd <ysjsw@ajou.ac.kr>
2025-08-18 22:50:12 +0900

    feat: modify the model to fit db setting

--- models.py
+++ models.py
@@ -1,20 +1,35 @@
-from pydantic import BaseModel
-from typing import List, Optional, Any
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from datetime import datetime
 
+# Pydantic 모델 기본 설정
+class MongoBaseModel(BaseModel):
+    model_config = {
+        "validate_by_name": True,
+        "arbitrary_types_allowed": True
+    }
+
+# API 응답 모델
 class ApiResponse(BaseModel):
     code: int
     message: str
-    data: Optional[Any] = None # data 필드는 어떤 타입이든 될 수 있으므로 Any 사용
+    data: Optional[dict] = None
 
-class Job(BaseModel):
-    id: int
-    timestamp: str
-    status: str
-    logs: Optional[List[str]] = None
+class Gpu(MongoBaseModel):
+    id: int = Field(default=0, alias="_id")
+    capacity: int
+    isAvailable: bool = True
+
+class Job(MongoBaseModel):
+    id: int = Field(default=0, alias="_id")
+    timestamp: datetime = Field(default_factory=datetime.utcnow)
+    status: str = "pending"
+    log: Optional[str] = None
     jobName: str
-    projectPath: Optional[str] = None
-    venvPath: Optional[str] = None
-    mainFile: Optional[str] = None
+    projectPath: str
+    venvPath: str
+    mainFile: str
+    gpuId: Optional[int] = None  # 배정된 GPU ID
 
 class JobCreate(BaseModel):
     jobName: str
@@ -27,6 +42,10 @@ class JobResponse(ApiResponse):
 
 class JobListResponse(ApiResponse):
     data: Optional[List[Job]] = None
+    
+class JobQueue(MongoBaseModel):
+    id: int = Field(default=0, alias="_id")
+    queue: List[int] = [] 
 
 class GpuStatus(BaseModel):
     gpu24gbActive: int  #사용 중인 GPU 개수
@@ -34,10 +53,12 @@ class GpuStatus(BaseModel):
     gpu24gbAvailable: int   #사용 가능한 24gb gpu 갯수 
     gpu8gbAvailable: int
     jobsInQueue: int 
+    totalGpu24gb: int = 6
+    totalGpu8gb: int = 12    
 
 class GpuStatusResponse(ApiResponse):
     data: Optional[GpuStatus] = None
-    
+
 class FileItem(BaseModel):  #파일 또는 폴더의 기본 정보
     name: str
     is_directory: bool
